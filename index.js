363408cdb144ff8ff86bbf80d70fa570112c628a
Berkay Urun <berkayurun@outlook.com>
2022-12-08 17:11:14 +0300

    complete challenge 3.7

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const res = require("express/lib/response");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
