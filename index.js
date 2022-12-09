d63817e75336d75ccc6b36009094d7db90d36f34
Berkay Urun <berkayurun@outlook.com>
2022-12-09 14:56:38 +0300

    complete challenge 3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
